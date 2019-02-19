f6063d64fec4d6ad4f5162a6092165651259e8bc
Rämö <miiaramo@lm7-cs-46.local>
2019-02-19 20:45:52 +0200

    basic functionalities working somehow

--- file_drop.py
+++ file_drop.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-
 import os
 from flask import Flask, request
 from werkzeug.utils import secure_filename
-import sqlite3
 
 UPLOAD_FOLDER = './Filesystem/'
 
@@ -13,8 +11,11 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def get_files_in_system():
   return str(os.listdir("./Filesystem"))
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
+  if request.method == 'GET':
+    return get_files_in_system()
+  
   if request.method == 'POST':
     # check if a file is selected
     if 'file' not in request.files:
@@ -24,18 +25,13 @@ def upload_file():
       filename = secure_filename(file.filename)
       file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
       return 'file uploaded'
-    return '''
-    <!doctype html>
-    <title>File drop</title>
-    <h1>File drop</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+
+@app.route('/<name>', methods=['GET'])
+def file_get(name):
+  return 'Requested file is ' + name
 
 def main():
-    print(get_files_in_system())
+    print('File drop app')
 
 if __name__ == "__main__":
     main()
